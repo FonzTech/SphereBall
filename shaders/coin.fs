a25bad1caed9927bff9c3c6fd96d1fa5b8d7c99e
Alfonso <9438351+FonzTech@users.noreply.github.com>
2019-03-23 23:48:39 +0100

    Add comments

--- shaders/coin.fs
+++ shaders/coin.fs
@@ -5,11 +5,16 @@ uniform float angle;
 
 void main()
 {
+	// Get diffuse color
+	vec4 m = texture2D(tex, gl_TexCoord[0]);
+	
+	// Get normalized angle difference
 	float r = float(int(angle) % int(degrees(PI * 2.0)));
 	float f = r > 180.0 ? 0.5 - (r - 180.0) / 360.0 : r / 360.0;
 	
-	vec4 m = texture2D(tex, gl_TexCoord[0]);
+	// Mix diffuse color with white glow
 	vec3 c = mix(m, vec3(1.0), f);
 	
+	// Apply above computed color to fragment
     gl_FragColor = vec4(c, 1.0);
 }

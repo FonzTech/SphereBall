ca8c3a0541f96d088632fdf9e096caa5c66de847
Alfonso <9438351+FonzTech@users.noreply.github.com>
2019-03-23 23:48:18 +0100

    Improve shader code
    Useless code has been removed. A new and simple strategy is used.

--- shaders/pill.fs
+++ shaders/pill.fs
@@ -5,21 +5,16 @@ varying vec4 positionToWorld;
 
 void main()
 {
-	vec4 m = gl_TexCoord[0];
 	vec4 color = texture2D(tex, gl_TexCoord[0]);
 	
 	{
-		float f = clamp(distance(positionToWorld.y, position.y), 0.0, 5.0) / 5.0;
-		
-		float coeff = clamp((position.y - positionToWorld.y) / 5.0, 0.0, 1.0);
-		color.rgb = mix(color.rgb, vec3(0), coeff);
+		float coeff = clamp((positionToWorld.y - position.y) / 5.0, 0.0, 1.0);
+		color.rgb = mix(color.rgb, vec3(1), coeff);
 	}
 	
 	{
-		float f = clamp(distance(position.y, positionToWorld.y), 0.0, 5.0) / 5.0;
-		
-		float coeff = clamp((positionToWorld.y - position.y) / 5.0, 0.0, 1.0);
-		color.rgb = mix(color.rgb, vec3(1), coeff);
+		float coeff = clamp((position.y - positionToWorld.y) / 5.0, 0.0, 1.0);
+		color.rgb = mix(color.rgb, vec3(0), coeff);
 	}
 	
     gl_FragColor = color;

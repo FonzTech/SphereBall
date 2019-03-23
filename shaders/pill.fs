35a0f8dd5f45c8c9a1d4b01d310c6c6a0ec730ac
Alfonso <9438351+FonzTech@users.noreply.github.com>
2019-03-23 23:37:00 +0100

    Improve desired visual effect

--- shaders/pill.fs
+++ shaders/pill.fs
@@ -3,15 +3,24 @@ uniform vec3 position;
 
 varying vec4 positionToWorld;
 
-#define PI 3.1416
-
 void main()
 {
 	vec4 m = gl_TexCoord[0];
-	vec4 c = texture2D(tex, gl_TexCoord[0]);
+	vec4 color = texture2D(tex, gl_TexCoord[0]);
 	
-	float f = clamp(distance(positionToWorld.xyz, position) / 10.0, 0.0, 1.0);
-	c.rgb = mix(c.rgb, vec3(0), f);
+	{
+		float f = clamp(distance(positionToWorld.y, position.y), 0.0, 5.0) / 5.0;
+		
+		float coeff = clamp((position.y - positionToWorld.y) / 5.0, 0.0, 1.0);
+		color.rgb = mix(color.rgb, vec3(0), coeff);
+	}
 	
-    gl_FragColor = c;
+	{
+		float f = clamp(distance(position.y, positionToWorld.y), 0.0, 5.0) / 5.0;
+		
+		float coeff = clamp((positionToWorld.y - position.y) / 5.0, 0.0, 1.0);
+		color.rgb = mix(color.rgb, vec3(1), coeff);
+	}
+	
+    gl_FragColor = color;
 }

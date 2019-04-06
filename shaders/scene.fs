2042cdb551fd26e0b74e55a4c079f52f9beb1e5f
Alfonso <9438351+FonzTech@users.noreply.github.com>
2019-04-06 17:00:47 +0200

    Fix effect on UV borders

--- shaders/scene.fs
+++ shaders/scene.fs
@@ -7,19 +7,26 @@ uniform float strength;
 vec2 sineWave(vec2 p)
 {
     // Wave distortion
-    float x = sin(20.0 * p.x + 40.0 * p.y + time * 0.005) * strength;
+    float x = cos(20.0 * p.x + 40.0 * p.y + time * 0.005) * strength;
     float y = sin(40.0 * p.x + 20.0 * p.y + time * 0.005) * strength;
-    return vec2(p.x + x, p.y + y);
+    return vec2(clamp(p.x + x, 0.001, 0.999), clamp(p.y + y, 0.001, 0.999));
 }
 
 void main()
 {
 	// Apply wave effect
-	vec2 coord = sineWave(gl_TexCoord[0]);
+	vec2 coord;
+	if (strength < 0.00001)
+	{
+		coord = gl_TexCoord[0];
+	}
+	else
+	{
+		coord = sineWave(gl_TexCoord[0]);
+	}
 
 	// Apply texture for the fragment
 	vec4 color = texture2D(tex, coord);
-	
 	
 	// Set fragment color
     gl_FragColor = color;
